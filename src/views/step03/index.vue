7d3af5c19dfa60ae23e5ca5db54184c877a3d835
Naoki Haba <naosprintrunner6385@gmail.com>
2024-03-20 20:58:14 +0900

    Add reactive variable for dynamic style control in step03 view

---
+++ src/views/step03/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const titleClass = ref('title')
+
+</script>
+
+<template>
+  <main>
+    <h1 :class="titleClass">
+      Make me red
+    </h1>
+  </main>
+</template>
+
+<style scoped>
+
+.title {
+  color: red;
+}
+</style>
