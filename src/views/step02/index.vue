c43cf2809983295f2127175638cfa7f7b15b0fac
Naoki Haba <naosprintrunner6385@gmail.com>
2024-03-20 20:57:55 +0900

    Add step02 view to the Vue application

---
+++ src/views/step02/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import { reactive, ref } from "vue"
+
+const count = reactive({
+  value: 0
+})
+
+const message = ref("Hello Vue 3")
+
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+    <button @click="count.value++">Count is: {{ count.value }}</button>
+  </main>
+</template>
