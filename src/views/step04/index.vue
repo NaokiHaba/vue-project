8af501b9daa54570549b44bb6b17f735bf949a65
Naoki Haba <naosprintrunner6385@gmail.com>
2024-03-20 20:58:23 +0900

    Add counter functionality in step04 view

---
+++ src/views/step04/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <main>
+    <h1>Count is: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+  </main>
+</template>
+
+<style scoped>
+</style>
