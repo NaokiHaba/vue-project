044679485b2c065fadfeb3fa962946dfbd1f2598
Naoki Haba <naosprintrunner6385@gmail.com>
2024-03-20 20:51:42 +0900

    Remove default Vue components and update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ReactiveView from '@/views/step02/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/step02',
+            name: 'step02',
+            component: ReactiveView
+        },
+        {
+            path: '/step03',
+            name: 'step03',
+            component: () => import('@/views/step03/index.vue')
+        },
+        {
+            path: '/step04',
+            name: 'step04',
+            component: () => import('@/views/step04/index.vue')
+        },
+    ]
 })
 
 export default router
